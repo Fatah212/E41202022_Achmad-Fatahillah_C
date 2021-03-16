1296d231953e3d3802caa43bd141bc471fd051ca
Achmad Fatahillah <Achmadfatah073@gmail.com>
2021-03-16 17:19:03 +0700

    W02_Latihan

---
+++ WSIBD_Latihan/W02_Latihan/src/w02_latihan/Latihan4.java
@@ -0,0 +1,19 @@
+package w02_latihan;
+
+import java.util.Scanner;
+
+public class Latihan4 {
+
+    public static void main(String[] args) {
+       Scanner s1 = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai1 = ");
+        int nilai1 = s1.nextInt();
+        System.out.print("Masukkan nilai2 = ");
+        int nilai2 = s1.nextInt();
+        
+        int jumlah = nilai1 + nilai2;
+        System.out.println("Jumlah = " + jumlah);
+    }
+
+}
---
+++ WSIBD_Latihan/W02_Latihan/src/w02_latihan/W02_Latihan.java
@@ -0,0 +1,13 @@
+package w02_latihan;
+
+public class W02_Latihan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("World !");
+
+        System.out.print("Teknik ");
+        System.out.print("Informatika !");
+    }
+
+}
