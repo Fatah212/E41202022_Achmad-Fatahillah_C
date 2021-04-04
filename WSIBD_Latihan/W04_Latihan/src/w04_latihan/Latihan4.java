8577f62c9deee660b462f4d2595279004bdba1b3
Achmad Fatahillah <Achmadfatah073@gmail.com>
2021-04-04 21:08:12 +0700

    Minggu4

---
+++ WSIBD_Latihan/W04_Latihan/src/w04_latihan/Latihan4.java
@@ -0,0 +1,11 @@
+package w04_latihan;
+
+public class Latihan4 {
+
+    public static void main(String[] args) {
+        int angka[] = {3, 1, 42, 24, 12};
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+}
