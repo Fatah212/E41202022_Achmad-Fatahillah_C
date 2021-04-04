67989a0f918ed07ab73fabf43261ef30f4c7575e
Achmad Fatahillah <Achmadfatah073@gmail.com>
2021-04-04 20:51:29 +0700

    Minggu3

---
+++ WSIBD_Latihan/W04_Latihan/src/w04_latihan/Latihan2.java
@@ -0,0 +1,13 @@
+package w04_latihan;
+
+public class Latihan2 {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while (i <= 10);
+    }
+}
